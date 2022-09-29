53df49bd16261fc3fcdfd69254eede8ee1fbab28
Tsypin Ilya <ilyaboy@list.ru>
2022-09-29 21:05:44 +0300

    Add Exceptions for future shapes

---
+++ ShapesLibrary/Exceptions/UnsupportedShapeException.cs
@@ -0,0 +1,8 @@
+﻿namespace ShapesLibrary.Exceptions;
+
+public class UnsupportedShapeException : ShapeException
+{
+    public UnsupportedShapeException(string message) : base(message)
+    {
+    }
+}
